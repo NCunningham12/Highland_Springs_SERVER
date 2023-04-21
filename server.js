0a790d3b6c6e9ef111307e52ce4c4757e92925ca
Nathan Cunningham <counterweight22@gmail.com>
2023-04-20 21:24:15 -0700

    Sign-up / Password Salt-Hashing

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mysql = require('mysql2');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +14,7 @@ const db = mysql.createConnection({
   database: 'highland_springs_golf',
 });
 
+// Adding New Members
 app.post('/add-member', (req, res) => {
   const first = req.body.first;
   const last = req.body.last;
@@ -34,6 +36,46 @@ app.post('/add-member', (req, res) => {
     }
   );
 });
+
+// Getting Member usernames/passwords
+app.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+// Sign-up
+app.post('/users', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const username = req.body.username;
+    const password = hashedPassword;
+
+    db.query(
+      'INSERT INTO users (username, password) VALUES (?,?)',
+      [username, password],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send('User Created');
+        }
+      }
+    );
+  } catch {
+    res.status(500).send();
+  }
+});
+
+// Login
+app.post('/users/login', async (req, res) => {
+  
+})
 
 app.listen(3001, () => {
   console.log('Server Running on Port 3001');
